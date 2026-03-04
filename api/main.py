2032a638ee0549874b22d9690a5b0d47d2923bda
Jayvee P. Crujido <jvcrujido@gmail.com>
2026-03-04 22:26:30 +0800

    Added a URL in 'allowed_origin'

--- api/main.py
+++ api/main.py
@@ -18,7 +18,7 @@ def create_app() -> FastAPI:
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:8000"],
+        allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:8000", "https://nurturing-bravery-production.up.railway.app"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
